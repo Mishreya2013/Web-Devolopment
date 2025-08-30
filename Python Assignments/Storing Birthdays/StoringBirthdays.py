8ddfa2cc6cebb67fa50f687e2a05e10091826ecd
Mishreya2013 <sumantwaghmare@gmail.com>
2025-08-30 22:39:22 +0900

    Storing Birthdays

---
+++ Python Assignments/Storing Birthdays/StoringBirthdays.py
@@ -0,0 +1,10 @@
+name_Aanya= "August 25 2011"
+print(name_Aanya)
+_NameIshika= "September 25 2011"
+print(_NameIshika)
+Momsbirthday= "The Eighteenth of April 1980 "
+print(Momsbirthday)
+papasbirthday= "June 20 1978"
+print(papasbirthday)
+MYBESTFRIENDSBIRTHDAY= "17th May 2013"
+print(MYBESTFRIENDSBIRTHDAY)
